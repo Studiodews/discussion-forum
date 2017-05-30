69843b650056a06f7acb4fc3e936d7dbae04185d
mohamed hussein <m.hussein1389@gmail.com>
2017-05-31 00:17:31 +0300

    implement sigin function

---
+++ includes/loginfunctions.php
@@ -0,0 +1,39 @@
+<?php
+include'dbconfig.php';
+
+/**
+ *login to forum if username and password match
+ * @param string username
+ * @param string password
+ * */
+ 
+ function signin($username,$password)
+ {
+    Global $error;
+    $username=mysql_real_escape_string($username);
+    $password=mysql_real_escape_string($password);
+    
+    //calculate md5 hash for password
+    $password=md5($password);
+    
+    $sql="SELECT * FROM fusers WHERE userName ='$username' AND password='$password'";
+    
+    $result=mysql_query($sql)or die("query failed ".mysql_error());
+    if(mysql_num_rows($result)==1)
+    {
+        $_SESSION['athorized']=true;
+        $row=mysql_fetch_assoc($result);
+        $_SESSION['userID']=$row->userID;
+        header("location:../");
+        exit();
+    }
+    else
+    {
+        $error[]="wrong username or password";
+        //echo"wrong username or password";
+    }
+ }
+ 
+ //signin("admin","admin"); 
+
+?>
