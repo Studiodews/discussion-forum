5dfa901cbb456bad897b606a4fda313b788f4f20
mohamed hussein <m.hussein1389@gmail.com>
2017-05-30 22:44:05 +0300

    add database connection script

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -20,4 +20,11 @@ define("DBUSER","root");
 define("DBPASSWORD","");
 define("DBNAME","mydb");
 
+
+//connect to mysql
+$con =mysql_connect(DBHOST,DBUSER,DBPASSWORD)or die("connection to database failed ".Mysql_error());
+//select database to work on 
+$con=mysql_select_db(DBNAME,$con) or die("select database failed ".Mysql_error());
+
+//echo "connection done";
 ?>
