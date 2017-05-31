3c357c77dcd8e6b323f7b75e93671521c6c47eff
mohamed hussein <m.hussein1389@gmail.com>
2017-05-31 18:03:59 +0300

    implement category add script

--- newcat.php
+++ newcat.php
@@ -7,5 +7,20 @@ if(!isSignedIn())
   header('location:./signin.php');
   exit();
 }
+
+if(isset($_POST['submit']))
+{
+    $catName=mysql_real_escape_string($_POST['catName']);
+    $catDesc=mysql_real_escape_string($_POST['catDesc']);
+     $sql="INSERT INTO fcategories (catName,catDesc)VALUES
+         ('$catName','$$catDesc')";
+         
+       mysql_query($sql)or die("query failed ".mysql_error());  
+}
+else
+{
+    
+}
+
 include 'layout/footer.php';
 ?>
