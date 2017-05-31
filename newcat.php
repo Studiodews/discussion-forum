1f526ae9a0fdf4a5a3227969542554802fabc72e
mohamed hussein <m.hussein1389@gmail.com>
2017-05-31 18:17:20 +0300

    implement category add form script

--- newcat.php
+++ newcat.php
@@ -13,13 +13,20 @@ if(isset($_POST['submit']))
     $catName=mysql_real_escape_string($_POST['catName']);
     $catDesc=mysql_real_escape_string($_POST['catDesc']);
      $sql="INSERT INTO fcategories (catName,catDesc)VALUES
-         ('$catName','$$catDesc')";
+         ('$catName','$catDesc')";
          
-       mysql_query($sql)or die("query failed ".mysql_error());  
+       mysql_query($sql)or die("query failed ".mysql_error()); 
+       header('location:./');
+        exit();
 }
 else
 {
-    
+    echo '<form action="" method="post" id="newCat">
+    <p><label><strong>Category Name </strong></label> <input type="text" placeholder="enter category name " name="catName" /></p>
+    <strong>Category Description </strong></label>
+    <p><textarea name="catDesc" rows="8" cols="60" placeholder="Type category description "></textarea></p>
+    <input type="submit" name="submit" value="submit" id="button" />
+    </form>';
 }
 
 include 'layout/footer.php';
